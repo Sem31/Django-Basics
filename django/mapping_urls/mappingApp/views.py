2915a1d1c476176fa8d8b21123b6f705fc0d490d
Sem31 <31kamleshprajapat@gmail.com>
2019-06-04 18:23:07 +0530

    mapp the urls in this project

---
+++ django/mapping_urls/mappingApp/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def index(request) :
+    return HttpResponse("Hello Sem")
+
+
+def index1(request) :
+    return HttpResponse("Hello Sem123")
