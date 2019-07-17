f7fe82405c17f6857dd47a91b5bf1cb0541bcde9
Sem31 <31kamleshprajapat@gmail.com>
2019-07-18 01:28:37 +0530

    some changes in the models and database

--- django/models_and_database/first_model/views.py
+++ django/models_and_database/first_model/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from first_model.models import AccessRecord,Topic,Webpage
 # Create your views here.
+
+def index(request) :
+    webpage_list = AccessRecord.objects.order_by('date')
+    date_dict = {'AccessRecords' : webpage_list}
+    return render(request,'index.html', context = date_dict)
